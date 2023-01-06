4e8962ea1cdd698387771dd94caac2d1def95eb3
Shrey Kukshal <shrey@synergy.co.in>
2023-01-06 19:52:30 +0530

    added route Table association

---
+++ modules/routeTableAssociation/main.tf
@@ -0,0 +1,21 @@
+resource "aws_route_table_association" "bastionSubnet" {
+  subnet_id      = var.bastionSubnetId
+  route_table_id = var.bastionRtId
+}
+
+resource "aws_route_table_association" "mainPublicSubnetA" {
+  subnet_id      = var.mainPulicSubnetId
+  route_table_id = var.mainPublicRtId
+}
+
+resource "aws_route_table_association" "mainPrivateSubnetA" {
+  count = length(var.mainPrivateSubnetAId)
+  subnet_id      = tolist(var.mainPrivateSubnetAId)[count.index]
+  route_table_id = var.mainPrivateRtId
+}
+
+resource "aws_route_table_association" "mainPrivateSubnetB" {
+count = length(var.mainPrivateSubnetAId)
+  subnet_id      = tolist(var.mainPrivateSubnetAId)[count.index]
+  route_table_id = var.mainPrivateRtId
+}
---
+++ modules/routeTableAssociation/variable.tf
@@ -0,0 +1,25 @@
+variable "bastionSubnetId" {
+  type = any
+}
+variable "bastionRtId" {
+  type = any
+}
+
+#main
+variable "mainPulicSubnetId" {
+  type = any
+}
+variable "mainPublicRtId" {
+  type = any
+}
+
+variable "mainPrivateSubnetAId" {
+  type = any
+}
+variable "mainPrivateRtId" {
+  type = any
+}
+
+variable "mainPrivateSubnetBId" {
+  type = any
+}
